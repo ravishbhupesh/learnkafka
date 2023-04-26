af0d25c70ccbf5c6bc129a039b1f3c2af7e2e726
= <ravishbhupesh@gmail.com>
2023-04-26 18:20:46 +0200

    kafka spring example updated

--- kafka-samples/kafka-spring-example/src/main/java/br/learn/kafka/spring/listener/KafkaTopicListener.java
+++ kafka-samples/kafka-spring-example/src/main/java/br/learn/kafka/spring/listener/KafkaTopicListener.java
@@ -9,11 +9,28 @@ import org.springframework.stereotype.Service;
 public class KafkaTopicListener {
 
 	@Value("${topic.name.consumer}")
-    private String topicName;
+	private String topicName;
 
-    @KafkaListener(topics = "${topic.name.consumer}", groupId = "${spring.kafka.consumer.group-id}")
-    public void processEvent(ConsumerRecord<String, String> payload){
-    	System.out.println("New event available on topic : " + topicName + ", Content : " + payload.value());
-
+	@KafkaListener(topics = "${topic.name.consumer}", groupId = "${spring.kafka.consumer.group-id}")
+	public void processEvent(ConsumerRecord<String, String> payload) {
+		System.out.println("New event available on topic : " + topicName + ", Content : " + payload.value());
+		System.out.println(payload.value() + " is a prime number ? : " + isPrime(Long.parseLong(payload.value())));
+	}
+	
+	private boolean isPrime(long input) {
+    	
+    	long absInput = Math.abs(input);
+    	
+    	if( 0 == absInput || 1 == absInput) {
+    		return false;
+    	} else {
+    		for(long i = 2; i < absInput; i++) {
+    			if(absInput % i == 0) {
+    				return false;
+    			}
+    		}
+    	}
+    	
+    	return true;
     }
 }
