7d04ac6a7a0d247ebdc6e181dd0e87ca5a522bd0
Bhupesh <>
2023-05-01 10:11:34 +0200

    kafka spring listener

--- kafka-samples/kafka-spring-example/src/main/java/br/learn/kafka/spring/Application.java
+++ kafka-samples/kafka-spring-example/src/main/java/br/learn/kafka/spring/Application.java
@@ -1,16 +1,45 @@
 package br.learn.kafka.spring;
 
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @EnableKafka
 @SpringBootApplication
 public class Application {
-	
-	public static void main(String[] args) {
 
-		SpringApplication.run(Application.class, args);
-	}
+    @Value("${topic.name.consumer}")
+    private String topicName;
+
+    public static void main(String[] args) {
+
+	SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    NewTopic topic() {
+	return TopicBuilder.name(topicName)
+		.partitions(1)
+		.replicas(1)
+		.build();
+    }
+
+    @Bean
+    ApplicationRunner runner(KafkaTemplate<String, String> template) {
+	Random random = new Random();
+	return args -> {
+	    IntStream samples = random.ints(10, 1, 500);
+	    samples.forEach(s -> template.send(topicName, Long.toString(s)));
+	};
+    }
 
 }
--- kafka-samples/kafka-spring-example/src/main/java/br/learn/kafka/spring/listener/KafkaTopicListener.java
+++ kafka-samples/kafka-spring-example/src/main/java/br/learn/kafka/spring/listener/KafkaTopicListener.java
@@ -1,6 +1,8 @@
 package br.learn.kafka.spring.listener;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -8,29 +10,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaTopicListener {
 
-	@Value("${topic.name.consumer}")
-	private String topicName;
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaTopicListener.class);
 
-	@KafkaListener(topics = "${topic.name.consumer}", groupId = "${spring.kafka.consumer.group-id}")
-	public void processEvent(ConsumerRecord<String, String> payload) {
-		System.out.println("New event available on topic : " + topicName + ", Content : " + payload.value());
-		System.out.println(payload.value() + " is a prime number ? : " + isPrime(Long.parseLong(payload.value())));
+    @Value("${topic.name.consumer}")
+    private String topicName;
+
+    @KafkaListener(topics = "${topic.name.consumer}", groupId = "${spring.kafka.consumer.group-id}")
+    public void processEvent(ConsumerRecord<String, String> payload) {
+	LOGGER.info("New event available on topic : {}, Content : {}", topicName, payload.value());
+	LOGGER.info("Is {} a prime number ? : {}", payload.value(), isPrime(Long.parseLong(payload.value())));
+    }
+
+    private boolean isPrime(long input) {
+
+	long absInput = Math.abs(input);
+
+	if (0 == absInput || 1 == absInput) {
+	    return false;
+	} else {
+	    for (long i = 2; i < absInput; i++) {
+		if (absInput % i == 0) {
+		    return false;
+		}
+	    }
 	}
-	
-	private boolean isPrime(long input) {
-    	
-    	long absInput = Math.abs(input);
-    	
-    	if( 0 == absInput || 1 == absInput) {
-    		return false;
-    	} else {
-    		for(long i = 2; i < absInput; i++) {
-    			if(absInput % i == 0) {
-    				return false;
-    			}
-    		}
-    	}
-    	
-    	return true;
+	return true;
     }
 }
